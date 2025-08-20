ef0ea39fa8970bc231d3aa31245b0f8daadfb2b8
Deepak Dey <deepakdey412@gmail.com>
2025-08-20 23:29:34 +0530

    Project folder Added

---
+++ taskifypro/src/main/java/com/dtech/taskifypro/TaskifyproApplication.java
@@ -0,0 +1,13 @@
+package com.dtech.taskifypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskifyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskifyproApplication.class, args);
+	}
+
+}
